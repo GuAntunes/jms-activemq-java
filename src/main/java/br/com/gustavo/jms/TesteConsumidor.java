1f85d5bac6150bc541fc15b2564a3b0b161e8f46
Gustavo Antunes <gas.antunes84@gmail.com>
2020-01-22 19:06:36 -0300

    Criado listener para a escuta de diversas mensagens

--- src/main/java/br/com/gustavo/jms/TesteConsumidor.java
+++ src/main/java/br/com/gustavo/jms/TesteConsumidor.java
@@ -1,11 +1,16 @@
 package br.com.gustavo.jms;
 
+import java.util.Scanner;
+
 import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
 import javax.jms.Destination;
+import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageConsumer;
+import javax.jms.MessageListener;
 import javax.jms.Session;
+import javax.jms.TextMessage;
 import javax.naming.InitialContext;
 
 public class TesteConsumidor {
@@ -14,26 +19,44 @@ public class TesteConsumidor {
 	public static void main(String[] args) throws Exception {
 
 		InitialContext context = new InitialContext();
-		
+
 		ConnectionFactory factory = (ConnectionFactory) context.lookup("ConnectionFactory");
 		Connection connection = factory.createConnection();
 		connection.start();
-		
-		/* O primeiro parâmetro do método createSession define se queremos usar o tratamento da 
-		 * transação como explícito. Como colocamos false, não é preciso chamar session.commit() ou session.rollback()
+
+		/*
+		 * O primeiro parâmetro do método createSession define se queremos usar o
+		 * tratamento da transação como explícito. Como colocamos false, não é preciso
+		 * chamar session.commit() ou session.rollback()
 		 */
 		Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-		
-		/* Sub Interfaces de Destination:
-		 * Queue, TemporaryQueue, TemporaryTopic, Topic.
+
+		/*
+		 * Sub Interfaces de Destination: Queue, TemporaryQueue, TemporaryTopic, Topic.
 		 */
-		Destination destination  = (Destination) context.lookup("financeiro");
+		Destination destination = (Destination) context.lookup("financeiro");
 		MessageConsumer consumer = session.createConsumer(destination);
+
+		consumer.setMessageListener(new MessageListener() {
+
+			public void onMessage(Message message) {
+				TextMessage textMessage = (TextMessage) message;
+				
+				try {
+					System.out.println("Recebendo a mensagem: " + textMessage.getText());
+				} catch (JMSException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
+
 		
-		Message message = consumer.receive();
+		new Scanner(System.in).nextLine();
 		
-		System.out.println("Recebendo a mensagem: " + message);
-		
+		// Recebe apenas uma mensagem
+//		Message message = consumer.receive();
+
 		session.close();
 		context.close();
 		connection.close();
